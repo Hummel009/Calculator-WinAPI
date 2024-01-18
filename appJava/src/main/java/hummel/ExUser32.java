1728acff486d89c97e04bde1196e1ce3b6141513
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-18 20:38:13 +0300

    1

--- appJava/src/main/java/hummel/ExUser32.java
+++ appJava/src/main/java/hummel/ExUser32.java
@@ -1,27 +1,12 @@
 package hummel;
 
 import com.sun.jna.Native;
-import com.sun.jna.Pointer;
-import com.sun.jna.Structure;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.win32.W32APIOptions;
 
 @SuppressWarnings({"FunctionName", "unused"})
 interface ExUser32 extends User32 {
 	ExUser32 INSTANCE = Native.load("user32", ExUser32.class, W32APIOptions.DEFAULT_OPTIONS);
-
-	@Structure.FieldOrder({"ptReserved", "ptMaxSize", "ptMaxPosition", "ptMinTrackSize", "ptMaxTrackSize"})
-	class MINMAXINFO extends Structure {
-		public POINT ptReserved;
-		public POINT ptMaxSize;
-		public POINT ptMaxPosition;
-		public POINT ptMinTrackSize;
-		public POINT ptMaxTrackSize;
-
-		public MINMAXINFO(Pointer pointer) {
-			super(pointer);
-		}
-	}
 
 	boolean SetWindowText(HWND hWnd, String lpString);
 }
