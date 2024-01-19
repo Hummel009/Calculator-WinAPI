0efbbc2da162798501fec62d4b3ce89f3ffd2dc6
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-19 14:48:11 +0300

    hier

---
+++ appCs/src/ApiFunctions.cs
@@ -0,0 +1,77 @@
+#pragma warning disable
+
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Hummel
+{
+    public class WinAPI
+    {
+        public delegate IntPtr WndProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int RegisterClass(ref WNDCLASS lpwcx);
+
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(SystemMetric smIndex);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CreateWindowEx(
+           int dwExStyle,
+           int lpClassName,
+           string? lpWindowName,
+           int dwStyle,
+           int x,
+           int y,
+           int nWidth,
+           int nHeight,
+           IntPtr hWndParent,
+           IntPtr hMenu,
+           IntPtr hInstance,
+           IntPtr lpParam
+        );
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CreateWindowEx(
+            int dwExStyle,
+            string? lpClassName,
+            string? lpWindowName,
+            int dwStyle,
+            int x,
+            int y,
+            int nWidth,
+            int nHeight,
+            IntPtr hWndParent,
+            IntPtr hMenu,
+            IntPtr hInstance,
+            IntPtr lpParam
+        );
+
+        [DllImport("user32.dll")]
+        public static extern int GetMessage(out MSG lpMsg, IntPtr hWnd, int wMsgFilterMin, int wMsgFilterMax);
+
+        [DllImport("user32.dll")]
+        public static extern bool TranslateMessage(ref MSG lpMsg);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr DispatchMessage(ref MSG lpmsg);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr DefWindowProc(IntPtr hWnd, int uMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowText(IntPtr hwnd, string? lpString);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder? lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern void PostQuitMessage(int exitCode);
+    }
+}
--- appCs/src/Program.cs
+++ appCs/src/Program.cs
@@ -1,14 +1,22 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Text;
 
 namespace Hummel
 {
     public class Program
     {
-        public const uint WM_COMMAND = 0x0111;
-        public const uint COLOR_WINDOW = 0x5;
-        public const uint WS_VISIBLE = 0x10000000;
-        public const uint WS_CAPTION = 0x00C00000;
-        public const uint WS_SYSMENU = 0x00080000;
+        public const int COLOR_WINDOW = 0x5;
+
+        public const int WM_COMMAND = 0x0111;
+        public const int WM_CREATE = 0x0001;
+        public const int WM_CLOSE = 0x0010;
+        public const int WM_DESTROY = 0x0002;
+
+        public const int WS_VISIBLE = 0x10000000;
+        public const int WS_CAPTION = 0x00C00000;
+        public const int WS_SYSMENU = 0x00080000;
+        public const int WS_TABSTOP = 0x10000;
+        public const int WS_CHILD = 0x40000000;
+        public const int WS_EX_CLIENTEDGE = 0x00000200;
 
         public const int BUTTON_0_ID = 0;
         public const int BUTTON_1_ID = 1;
@@ -36,22 +44,21 @@ namespace Hummel
         public const int BUTTON_UNARY_MINUS_ID = 23;
 
         public const int DEFAULT_CAPACITY = 100;
-        private static readonly IntPtr field;
-        private static readonly List<string>? data;
-        private static WinAPI.WndProc? delegWndProc;
 
-        private static readonly int[] FACTORIAL = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600];
+        private static IntPtr field;
+        private static List<string>? data;
+
+        public static readonly int[] FACTORIAL = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600];
 
         public static void Main(string[] args)
         {
             var className = "HummelCalculator";
             var windowTitle = "WinAPI";
-            delegWndProc = ParentWndProc;
 
-            var windowClass = new WinAPI.WNDCLASS
+            var windowClass = new WNDCLASS
             {
                 style = 0,
-                lpfnWndProc = delegWndProc,
+                lpfnWndProc = WndProc,
                 cbClsExtra = 0,
                 cbWndExtra = 0,
                 hInstance = 0,
@@ -64,18 +71,18 @@ namespace Hummel
 
             var atom = WinAPI.RegisterClass(ref windowClass);
 
-            var screenWidth = WinAPI.GetSystemMetrics(WinAPI.SystemMetric.SM_CXSCREEN);
-            var screenHeight = WinAPI.GetSystemMetrics(WinAPI.SystemMetric.SM_CYSCREEN);
+            var screenWidth = WinAPI.GetSystemMetrics(SystemMetric.SM_CXSCREEN);
+            var screenHeight = WinAPI.GetSystemMetrics(SystemMetric.SM_CYSCREEN);
 
-            var windowWidth = 260;
-            var windowHeight = 458;
+            var windowWidth = 257;
+            var windowHeight = 449;
 
             var windowX = Math.Max(0, (screenWidth - windowWidth) / 2);
             var windowY = Math.Max(0, (screenHeight - windowHeight) / 2);
 
             WinAPI.CreateWindowEx(0, atom, windowTitle, WS_VISIBLE | WS_CAPTION | WS_SYSMENU, windowX, windowY, windowWidth, windowHeight, 0, 0, 0, 0);
 
-            var msg = new WinAPI.MSG();
+            var msg = new MSG();
             while (WinAPI.GetMessage(out msg, 0, 0, 0) != 0)
             {
                 WinAPI.TranslateMessage(ref msg);
@@ -83,10 +90,303 @@ namespace Hummel
             }
         }
 
-        private static IntPtr ParentWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        private static IntPtr WndProc(IntPtr window, int msg, IntPtr wParam, IntPtr lParam)
         {
-            return WinAPI.DefWindowProc(hWnd, msg, wParam, lParam);
+            switch (msg)
+            {
+                case WM_CREATE:
+                    data = [];
+                    field = RegisterField(window);
+
+                    RegisterButton(window, BUTTON_PI_ID, "p", 0, 0);
+                    RegisterButton(window, BUTTON_E_ID, "e", 1, 0);
+                    RegisterButton(window, BUTTON_C_ID, "C", 2, 0);
+                    RegisterButton(window, BUTTON_FACTORIAL_ID, "x!", 3, 0);
+                    RegisterButton(window, BUTTON_INVERSE_ID, "1/x", 0, 1);
+                    RegisterButton(window, BUTTON_SQUARE_ID, "x^2", 1, 1);
+                    RegisterButton(window, BUTTON_SQUARE_ROOT_ID, "sqrt(x)", 2, 1);
+                    RegisterButton(window, BUTTON_DIVIDE_ID, "/", 3, 1);
+                    RegisterButton(window, BUTTON_7_ID, "7", 0, 2);
+                    RegisterButton(window, BUTTON_8_ID, "8", 1, 2);
+                    RegisterButton(window, BUTTON_9_ID, "9", 2, 2);
+                    RegisterButton(window, BUTTON_MULTIPLE_ID, "*", 3, 2);
+                    RegisterButton(window, BUTTON_4_ID, "4", 0, 3);
+                    RegisterButton(window, BUTTON_5_ID, "5", 1, 3);
+                    RegisterButton(window, BUTTON_6_ID, "6", 2, 3);
+                    RegisterButton(window, BUTTON_MINUS_ID, "-", 3, 3);
+                    RegisterButton(window, BUTTON_1_ID, "1", 0, 4);
+                    RegisterButton(window, BUTTON_2_ID, "2", 1, 4);
+                    RegisterButton(window, BUTTON_3_ID, "3", 2, 4);
+                    RegisterButton(window, BUTTON_PLUS_ID, "+", 3, 4);
+                    RegisterButton(window, BUTTON_UNARY_MINUS_ID, "-", 0, 5);
+                    RegisterButton(window, BUTTON_0_ID, "0", 1, 5);
+                    RegisterButton(window, BUTTON_DOT_ID, ",", 2, 5);
+                    RegisterButton(window, BUTTON_EQUALS_ID, "=", 3, 5);
+                    break;
+                case WM_COMMAND:
+                    var buttonId = Loword(wParam);
+
+                    switch (buttonId)
+                    {
+                        case BUTTON_0_ID:
+                            PushSymbolWrapper("0");
+                            break;
+                        case BUTTON_1_ID:
+                            PushSymbolWrapper("1");
+                            break;
+                        case BUTTON_2_ID:
+                            PushSymbolWrapper("2");
+                            break;
+                        case BUTTON_3_ID:
+                            PushSymbolWrapper("3");
+                            break;
+                        case BUTTON_4_ID:
+                            PushSymbolWrapper("4");
+                            break;
+                        case BUTTON_5_ID:
+                            PushSymbolWrapper("5");
+                            break;
+                        case BUTTON_6_ID:
+                            PushSymbolWrapper("6");
+                            break;
+                        case BUTTON_7_ID:
+                            PushSymbolWrapper("7");
+                            break;
+                        case BUTTON_8_ID:
+                            PushSymbolWrapper("8");
+                            break;
+                        case BUTTON_9_ID:
+                            PushSymbolWrapper("9");
+                            break;
+                        case BUTTON_E_ID:
+                            PushSymbolWrapper("2.72");
+                            break;
+                        case BUTTON_PI_ID:
+                            PushSymbolWrapper("3.14");
+                            break;
+                        case BUTTON_DOT_ID:
+                            PushSymbolWrapper(",");
+                            break;
+                        case BUTTON_UNARY_MINUS_ID:
+                            PushSymbolWrapper("-");
+                            break;
+                        case BUTTON_C_ID:
+                            data!.Clear();
+                            WinAPI.SetWindowText(field, "");
+                            break;
+                        case BUTTON_DIVIDE_ID:
+                            PushOperation("/");
+                            break;
+                        case BUTTON_MULTIPLE_ID:
+                            PushOperation("*");
+                            break;
+                        case BUTTON_MINUS_ID:
+                            PushOperation("-");
+                            break;
+                        case BUTTON_PLUS_ID:
+                            PushOperation("+");
+                            break;
+                        case BUTTON_FACTORIAL_ID:
+                            PushOperation("!");
+                            break;
+                        case BUTTON_SQUARE_ID:
+                            PushOperation("s");
+                            break;
+                        case BUTTON_INVERSE_ID:
+                            PushOperation("i");
+                            break;
+                        case BUTTON_SQUARE_ROOT_ID:
+                            PushOperation("r");
+                            break;
+
+                        case BUTTON_EQUALS_ID:
+                            CalculateWrapper();
+                            break;
+                    }
+                    break;
+                case WM_CLOSE:
+                    WinAPI.DestroyWindow(window);
+                    break;
+                case WM_DESTROY:
+                    WinAPI.PostQuitMessage(0);
+                    break;
+            }
+            return WinAPI.DefWindowProc(window, msg, wParam, lParam);
         }
 
+        private static void CalculateWrapper()
+        {
+            try
+            {
+                Calculate(field);
+            }
+            catch (Exception)
+            {
+                data!.Clear();
+                WinAPI.SetWindowText(field, "Error!");
+            }
+        }
+
+        private static void Calculate(IntPtr field)
+        {
+            StringBuilder buffer = new(WinAPI.GetWindowTextLength(field) + 1);
+            WinAPI.GetWindowText(field, buffer, buffer.Capacity);
+            var str = buffer.ToString();
+
+            if (data!.Count == 2)
+            {
+                var op = data[1];
+
+                if (new HashSet<string> { "+", "-", "*", "/" }.Contains(op))
+                {
+                    data.Add(str);
+
+                    var operand1 = double.Parse(data[0]);
+                    var operand2 = double.Parse(data[2]);
+
+                    double result;
+                    switch (op)
+                    {
+                        case "+":
+                            result = operand1 + operand2;
+                            break;
+                        case "-":
+                            result = operand1 - operand2;
+                            break;
+                        case "*":
+                            result = operand1 * operand2;
+                            break;
+                        case "/":
+                            result = operand1 / operand2;
+                            break;
+                        default:
+                            throw new Exception("Invalid operator: " + op);
+                    };
+
+                    data.Clear();
+
+                    WinAPI.SetWindowText(field, result.ToString());
+                }
+                else if (new HashSet<string> { "!", "s", "i", "r" }.Contains(op))
+                {
+                    var operand = double.Parse(data[0]);
+                    double result;
+                    switch (op)
+                    {
+                        case "!":
+                            result = FACTORIAL[(int)operand];
+                            break;
+                        case "s":
+                            result = operand * operand;
+                            break;
+                        case "i":
+                            result = 1.0 / operand;
+                            break;
+                        case "r":
+                            result = Math.Sqrt(operand);
+                            break;
+                        default:
+                            throw new Exception("Invalid operator: " + op);
+                    };
+
+                    data.Clear();
+
+                    WinAPI.SetWindowText(field, result.ToString());
+                }
+            }
+        }
+
+        private static void PushOperation(String operation)
+        {
+            StringBuilder buffer = new(WinAPI.GetWindowTextLength(field) + 1);
+            WinAPI.GetWindowText(field, buffer, buffer.Capacity);
+            var str = buffer.ToString();
+
+            if (data!.Count == 0)
+            {
+                data.Add(str);
+                data.Add(operation);
+                WinAPI.SetWindowText(field, "");
+            }
+            else
+            {
+                data.Clear();
+                WinAPI.SetWindowText(field, "Error!");
+            }
+        }
+
+        private static void PushSymbolWrapper(String symbol)
+        {
+            StringBuilder buffer = new(WinAPI.GetWindowTextLength(field) + 1);
+            WinAPI.GetWindowText(field, buffer, buffer.Capacity);
+            var str = buffer.ToString();
+
+            switch (symbol)
+            {
+                case "3.14":
+                case "2.72":
+                case "-":
+                    if (string.IsNullOrEmpty(str))
+                    {
+                        PushSymbol(symbol);
+                    }
+                    break;
+
+                case ".":
+                    if (!str.Contains('.') && !string.IsNullOrEmpty(str))
+                    {
+                        PushSymbol(symbol);
+                    }
+                    break;
+                default:
+                    if (data!.Count == 0)
+                    {
+                        PushSymbol(symbol);
+                    }
+                    else
+                    {
+                        var op = data[1];
+
+                        if (!new HashSet<string> { "!", "s", "i", "r" }.Contains(op))
+                        {
+                            PushSymbol(symbol);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private static void PushSymbol(String symbol)
+        {
+            StringBuilder buffer = new(WinAPI.GetWindowTextLength(field) + 1);
+            WinAPI.GetWindowText(field, buffer, buffer.Capacity);
+            var str = buffer.ToString();
+            if ("Error!" == str)
+            {
+                WinAPI.SetWindowText(field, symbol);
+            }
+            else
+            {
+                WinAPI.SetWindowText(field, str + symbol);
+            }
+        }
+
+        private static IntPtr RegisterField(IntPtr window)
+        {
+            return WinAPI.CreateWindowEx(WS_EX_CLIENTEDGE, "STATIC", "", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 1, 1, 239, 48, window, 0, 0, 0);
+        }
+
+        private static void RegisterButton(IntPtr window, int id, String text, int gridX, int gridY)
+        {
+            var buttonWidth = 60;
+            var buttonHeight = 60;
+
+            WinAPI.CreateWindowEx(WS_EX_CLIENTEDGE, "BUTTON", text, WS_TABSTOP | WS_VISIBLE | WS_CHILD, buttonWidth * gridX + 1, buttonHeight * gridY + 50, buttonWidth, buttonHeight, window, new((long)id), 0, 0);
+        }
+
+        private static int Loword(IntPtr wparam)
+        {
+            return (int)(wparam.ToInt64() & 0xFFFF);
+        }
     }
 }
