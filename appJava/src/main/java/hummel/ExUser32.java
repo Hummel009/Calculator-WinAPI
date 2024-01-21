14572b483af4bbda56d2a16725ff6f9e12ba2fab
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-21 18:40:57 +0300

    Update ExUser32.java

--- appJava/src/main/java/hummel/ExUser32.java
+++ appJava/src/main/java/hummel/ExUser32.java
@@ -4,7 +4,7 @@ import com.sun.jna.Native;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.win32.W32APIOptions;
 
-@SuppressWarnings({"FunctionName", "unused"})
+@SuppressWarnings("FunctionName")
 interface ExUser32 extends User32 {
 	ExUser32 INSTANCE = Native.load("user32", ExUser32.class, W32APIOptions.DEFAULT_OPTIONS);
 
