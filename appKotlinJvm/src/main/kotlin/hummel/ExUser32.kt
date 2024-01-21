a183d82ab30b3edfa82b255f0e7e8e2f8d8c12f7
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-21 18:58:24 +0300

    Update ExUser32.kt

--- appKotlinJvm/src/main/kotlin/hummel/ExUser32.kt
+++ appKotlinJvm/src/main/kotlin/hummel/ExUser32.kt
@@ -5,7 +5,7 @@ import com.sun.jna.platform.win32.User32
 import com.sun.jna.platform.win32.WinDef.HWND
 import com.sun.jna.win32.W32APIOptions
 
-@Suppress("FunctionName", "unused")
+@Suppress("FunctionName")
 internal interface ExUser32 : User32 {
 	companion object {
 		val INSTANCE: ExUser32 = Native.load("user32", ExUser32::class.java, W32APIOptions.DEFAULT_OPTIONS)
