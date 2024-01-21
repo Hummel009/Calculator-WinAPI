18c63f51ae889ef33be12c1c511b6e08ea44e72e
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-01-21 19:00:04 +0300

    Update Program.cs

--- appCs/src/Program.cs
+++ appCs/src/Program.cs
@@ -4,51 +4,51 @@ namespace Hummel
 {
 	public class Program
 	{
-		public const int COLOR_WINDOW = 0x5;
+		private const int COLOR_WINDOW = 0x5;
 
-		public const int WM_COMMAND = 0x0111;
-		public const int WM_CREATE = 0x0001;
-		public const int WM_CLOSE = 0x0010;
-		public const int WM_DESTROY = 0x0002;
+		private const int WM_COMMAND = 0x0111;
+		private const int WM_CREATE = 0x0001;
+		private const int WM_CLOSE = 0x0010;
+		private const int WM_DESTROY = 0x0002;
 
-		public const int WS_VISIBLE = 0x10000000;
-		public const int WS_CAPTION = 0x00C00000;
-		public const int WS_SYSMENU = 0x00080000;
-		public const int WS_TABSTOP = 0x10000;
-		public const int WS_CHILD = 0x40000000;
-		public const int WS_EX_CLIENTEDGE = 0x00000200;
+		private const int WS_VISIBLE = 0x10000000;
+		private const int WS_CAPTION = 0x00C00000;
+		private const int WS_SYSMENU = 0x00080000;
+		private const int WS_TABSTOP = 0x10000;
+		private const int WS_CHILD = 0x40000000;
+		private const int WS_EX_CLIENTEDGE = 0x00000200;
 
-		public const int BUTTON_0_ID = 0;
-		public const int BUTTON_1_ID = 1;
-		public const int BUTTON_2_ID = 2;
-		public const int BUTTON_3_ID = 3;
-		public const int BUTTON_4_ID = 4;
-		public const int BUTTON_5_ID = 5;
-		public const int BUTTON_6_ID = 6;
-		public const int BUTTON_7_ID = 7;
-		public const int BUTTON_8_ID = 8;
-		public const int BUTTON_9_ID = 9;
-		public const int BUTTON_C_ID = 10;
-		public const int BUTTON_DIVIDE_ID = 11;
-		public const int BUTTON_DOT_ID = 12;
-		public const int BUTTON_EQUALS_ID = 13;
-		public const int BUTTON_E_ID = 14;
-		public const int BUTTON_FACTORIAL_ID = 15;
-		public const int BUTTON_INVERSE_ID = 16;
-		public const int BUTTON_MINUS_ID = 17;
-		public const int BUTTON_MULTIPLE_ID = 18;
-		public const int BUTTON_PI_ID = 19;
-		public const int BUTTON_PLUS_ID = 20;
-		public const int BUTTON_SQUARE_ID = 21;
-		public const int BUTTON_SQUARE_ROOT_ID = 22;
-		public const int BUTTON_UNARY_MINUS_ID = 23;
+		private const int BUTTON_0_ID = 0;
+		private const int BUTTON_1_ID = 1;
+		private const int BUTTON_2_ID = 2;
+		private const int BUTTON_3_ID = 3;
+		private const int BUTTON_4_ID = 4;
+		private const int BUTTON_5_ID = 5;
+		private const int BUTTON_6_ID = 6;
+		private const int BUTTON_7_ID = 7;
+		private const int BUTTON_8_ID = 8;
+		private const int BUTTON_9_ID = 9;
+		private const int BUTTON_C_ID = 10;
+		private const int BUTTON_DIVIDE_ID = 11;
+		private const int BUTTON_DOT_ID = 12;
+		private const int BUTTON_EQUALS_ID = 13;
+		private const int BUTTON_E_ID = 14;
+		private const int BUTTON_FACTORIAL_ID = 15;
+		private const int BUTTON_INVERSE_ID = 16;
+		private const int BUTTON_MINUS_ID = 17;
+		private const int BUTTON_MULTIPLE_ID = 18;
+		private const int BUTTON_PI_ID = 19;
+		private const int BUTTON_PLUS_ID = 20;
+		private const int BUTTON_SQUARE_ID = 21;
+		private const int BUTTON_SQUARE_ROOT_ID = 22;
+		private const int BUTTON_UNARY_MINUS_ID = 23;
 
-		public const int DEFAULT_CAPACITY = 100;
+		private const int DEFAULT_CAPACITY = 100;
 
 		private static IntPtr field;
 		private static List<string>? data;
 
-		public static readonly int[] FACTORIAL = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600];
+		private static readonly int[] FACTORIAL = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600];
 
 		public static void Main(string[] args)
 		{
