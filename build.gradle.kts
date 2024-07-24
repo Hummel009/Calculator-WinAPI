881983f030ea22ccd91f7c49fe1d6c4619784df7
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-24 20:25:31 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
 	id("org.jetbrains.kotlin.multiplatform") version "latest.release" apply false
 	id("org.jetbrains.kotlin.jvm") version "latest.release" apply false
-	id("org.openjfx.javafxplugin") version "latest.release" apply false
 }
